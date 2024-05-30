d2c40a768b9c2b886cddd19aac09ec8d6626df83
HeseltineBTutu <heseltinetutu@gmail.com>
2024-05-30 17:54:11 +0300

    Update: modified the function to load data from database instead of csv file for performance

--- data/loader.py
+++ data/loader.py
@@ -1,51 +1,27 @@
 """
-This module contains a function to load data from a file into a
+This module contains a function to load data from a database into a
 pandas DataFrame.
 
 Functions:
-    load_data(file_path): Load the data from a file path.
+    load_data(): Load the data from the database table.
 """
 
+from flask import Flask
 import pandas as pd
-import os
+
+from models.models import Rating
 
 
-def load_data(file_path='data/ratings.csv'):
+def load_data():
     """
-    Load the data from a file path into a pandas DataFrame.
+    Load data from the ratings table in the database.
 
-    This function reads a comma separated values (csv) file, where each row
-    represents a rating.
-    Each row in the file should have the following columns: 'user_id',
-    'item_id', 'rating', 'timestamp'.
-    Any additional columns in the file will be ignored.
-
-    Args:
-        file_path: str, the path to the file.
     Returns:
-        rating: pd.DataFrame, the data loaded from the file. The DataFrame has
-        the following columns:
-        - user_id: int, the ID of the user.
-        - item_id: int, the ID of the item.
-        - rating: int, the rating given by the user to the item.
-        - timestamp: int, the timestamp of the rating.
+        pandas.DataFrame: The loaded data from the ratings table.
     """
-    delimiter = ','  # Define the delimiter variable
-    encoding = 'utf-8'  # Define the encoding variable
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(f"File not found: {file_path}")
-    
-    dtypes = {'userId': int, 'movieId': int, 'rating': float}
+    ratings = Rating.query.all()
 
-    column_names = ['userId', 'movieId', 'rating', 'timestamp']
-    ratings = pd.read_csv(file_path, sep=delimiter, encoding=encoding,
-                          names=column_names, dtype=dtypes,
-                          usecols=['userId', 'movieId', 'rating', 'timestamp'], skiprows=1)
-
-    # Drop rows with missing values in key columns
-    ratings.dropna(subset=['userId', 'movieId', 'rating'], inplace=True)
-    # Convert timestamp to datetime format
-    ratings['timestamp'] = pd.to_datetime(ratings['timestamp'])
-
-
-    return ratings
+    # Convert SQLAlchemy result to pandas DataFrame
+    ratings_df = pd.DataFrame([(r.userId, r.movieId, r.rating, r.timestamp) for r in ratings],
+                              columns=['userId', 'movieId', 'rating', 'timestamp'])
+    return ratings_df
